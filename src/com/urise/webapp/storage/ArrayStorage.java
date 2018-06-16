f15dfe0bd0dd909d45b540502c09899ccb50444d
dsoccer1980 <dsoccer1980@gmail.com>
2018-06-16 19:48:35 +0300

    Refactoring

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,10 +7,12 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+public class ArrayStorage implements Storage {
+    private final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
+    @Override
     public void update(Resume r) {
         if (r == null) {
             System.out.println("Warning: Resume is null");
@@ -24,15 +26,17 @@ public class ArrayStorage {
         }
     }
 
+    @Override
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
+    @Override
     public void save(Resume r) {
         if (r == null) {
             System.out.println("Warning: Resume is null");
-        } else if (size == storage.length) {
+        } else if (size == STORAGE_LIMIT) {
             System.out.println("Warning: Resume was not inserted. Storage is full");
         } else if (getIndex(r.getUuid()) != -1) {
             System.out.println("Warning: Resume '" + r + "' already exists in storage");
@@ -41,6 +45,7 @@ public class ArrayStorage {
         }
     }
 
+    @Override
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
@@ -51,6 +56,7 @@ public class ArrayStorage {
         }
     }
 
+    @Override
     public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
@@ -65,10 +71,12 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+    @Override
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
+    @Override
     public int size() {
         return size;
     }
