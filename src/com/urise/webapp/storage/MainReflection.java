ac5ae5c67f1fdb62dde89eef7a84df6c2b1ff5fe
dsoccer1980 <dsoccer1980@gmail.com>
2018-06-24 20:09:19 +0300

    HW4: call method toString through reflection

---
+++ src/com/urise/webapp/storage/MainReflection.java
@@ -0,0 +1,22 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume("uuid1");
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+
+        Method methodToString = resume.getClass().getMethod("toString");
+        System.out.println(resume.toString());
+        System.out.println(methodToString.invoke(resume));
+    }
+}
