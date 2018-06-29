e3abd4517152be17ecfc0be26f7d504c59c92918
dsoccer1980 <dsoccer1980@gmail.com>
2018-06-29 17:52:06 +0300

    Refactorin in Exceptions: added text 'with uuid='

--- src/com/urise/webapp/exception/ExistStorageException.java
+++ src/com/urise/webapp/exception/ExistStorageException.java
@@ -3,6 +3,6 @@ package com.urise.webapp.exception;
 public class ExistStorageException extends StorageException {
 
     public ExistStorageException(String uuid) {
-        super("Resume " + uuid + " already exists", uuid);
+        super("Resume with uuid = " + uuid + " already exists", uuid);
     }
 }
--- src/com/urise/webapp/exception/NotExistStorageException.java
+++ src/com/urise/webapp/exception/NotExistStorageException.java
@@ -3,6 +3,6 @@ package com.urise.webapp.exception;
 public class NotExistStorageException extends StorageException {
 
     public NotExistStorageException(String uuid) {
-        super("Resume " + uuid + " does not exist", uuid);
+        super("Resume with uuid = " + uuid + " does not exist", uuid);
     }
 }
