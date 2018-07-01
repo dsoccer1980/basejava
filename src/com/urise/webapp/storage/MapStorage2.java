b24e741b61de47b2ae2d9f98c5ceaaf9554410dc
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-01 14:42:39 +0300

    Added class MapStorage2 with new search key

---
+++ src/com/urise/webapp/storage/MapStorage2.java
@@ -0,0 +1,59 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapStorage2 extends AbstractStorage {
+    protected Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return storage.values().stream().sorted(COMPARATOR_RESUME).collect(Collectors.toList());
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    @Override
+    protected Resume getSearchKey(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected void doSave(Object searchKey, Resume resume) {
+        storage.put(((Resume) searchKey).getUuid(), resume);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return (Resume) searchKey;
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsValue((Resume) searchKey);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove(((Resume) searchKey).getUuid());
+    }
+
+    @Override
+    protected void doUpdate(Object searchKey, Resume resume) {
+        storage.put(((Resume) searchKey).getUuid(), resume);
+
+    }
+
+}
---
+++ test/com/urise/webapp/storage/MapStorage2Test.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+
+public class MapStorage2Test extends AbstractStorageTest {
+
+    public MapStorage2Test() {
+        super(new MapStorage());
+    }
+
+}
