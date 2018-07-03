9987ea506b7f56d9bda10e7aa6a4ec7d15e3a14e
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-03 10:11:35 +0300

    reformatting RunAllTests

--- test/com/urise/webapp/storage/RunAllTests.java
+++ test/com/urise/webapp/storage/RunAllTests.java
@@ -5,14 +5,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(
-        {
-                ArrayStorageTest.class,
-                SortedArrayStorageTest.class,
-                ListStorageTest.class,
-                MapSearchKeyUuidStorageTest.class,
-                MapSearchKeyResumeStorageTest.class
-        })
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapSearchKeyUuidStorageTest.class,
+        MapSearchKeyResumeStorageTest.class
+})
 public class RunAllTests {
 
 }
