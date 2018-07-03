e3e1d8f53125f2e251bbbd560506175165aab387
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-03 19:30:50 +0300

    Refactoring method isExist in MapResumeStorage

--- src/com/urise/webapp/storage/MapResumeStorage.java
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -42,7 +42,7 @@ public class MapResumeStorage extends AbstractStorage {
 
     @Override
     protected boolean isExist(Object searchKey) {
-        return storage.containsValue((Resume) searchKey);
+        return searchKey != null;
     }
 
     @Override
