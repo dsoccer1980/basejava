438c7a3029d1747145cc07aa35fa654e8aaa04f8
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-18 14:18:08 +0300

    Add DateUtil to convert year, month to LocalDate

---
+++ src/com/urise/webapp/util/DateUtil.java
@@ -0,0 +1,14 @@
+package com.urise.webapp.util;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class DateUtil {
+
+    public static LocalDate of(int year, Month month) {
+        return LocalDate.of(year, month, 1);
+    }
+
+    private DateUtil() {
+    }
+}
