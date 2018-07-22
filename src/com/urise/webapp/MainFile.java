4b3f2496afa4b68b3321d3a2387775323e09ab0a
dsoccer1980 <dsoccer1980@gmail.com>
2018-07-22 10:29:23 +0300

    HW09:  recursive directory output with offset

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -6,21 +6,22 @@ import java.io.File;
 
 public class MainFile {
 
-    public static void printFiles(File dir) {
+    public static void printFiles(File dir, String offset) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    printFiles(file);
+                    System.out.println(offset + "D:" + file.getName());
+                    printFiles(file, offset + "  ");
+
                 } else {
-                    System.out.println(file.getName());
+                    System.out.println(offset + "   " + "F:" + file.getName());
                 }
             }
         }
     }
 
-
     public static void main(String[] args) {
-        printFiles(new File("."));
+        printFiles(new File("."), "");
     }
 }
