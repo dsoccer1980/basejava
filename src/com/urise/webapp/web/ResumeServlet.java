1afd9b7fbe4064a17d0dd817b0ba92d56013ebb3
dsoccer1980 <dsoccer1980@gmail.com>
2018-08-23 09:55:03 +0300

    Locate new SqlStorage to init() of ResumeServlet

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -6,14 +6,23 @@ import com.urise.webapp.model.*;
 import com.urise.webapp.storage.SqlStorage;
 import com.urise.webapp.storage.Storage;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class ResumeServlet extends HttpServlet {
+    private final Config config = Config.get();
+    private Storage storage;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        storage = new SqlStorage(config.getDbUrl(), config.getDbUser(), config.getDbPassword());
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
@@ -23,10 +32,14 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
 
-        final Config config = Config.get();
-        Storage storage = new SqlStorage(config.getDbUrl(), config.getDbUser(), config.getDbPassword());
         String uuid = request.getParameter("uuid");
         List<Resume> resumes;
+        response.getWriter().write("<TABLE border=1>");
+        response.getWriter().write("<TR>");
+        response.getWriter().write("<TD><B>uuid</B></TD>");
+        response.getWriter().write("<TD><B>fullname</B></TD>");
+        response.getWriter().write("</TR>");
+
         if (uuid == null) {
             resumes = storage.getAllSorted();
             for (Resume resume : resumes) {
@@ -39,50 +52,14 @@ public class ResumeServlet extends HttpServlet {
             }
             showResume(response, resume);
         }
+
+        response.getWriter().write("</TABLE>");
     }
 
     private void showResume(HttpServletResponse response, Resume resume) throws IOException {
-        response.getWriter().write(resume.getUuid() + "  " + resume.getFullName());
-        response.getWriter().write("<BR>");
-        Map<ContactType, String> contacts = resume.getContacts();
-        if (!contacts.isEmpty()) {
-            response.getWriter().write("Contacts:<BR>");
-            response.getWriter().write("<TABLE cellpadding=2>");
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                response.getWriter().write("<TR>");
-                response.getWriter().write("<TD>" + entry.getKey() + "</TD>");
-                response.getWriter().write("<TD>" + entry.getValue() + "</TD>");
-                response.getWriter().write("</TR>");
-            }
-            response.getWriter().write("</TABLE>");
-        }
-        Map<SectionType, Section> sections = resume.getSections();
-        if (!sections.isEmpty()) {
-            response.getWriter().write("Sections:<BR>");
-            response.getWriter().write("<TABLE cellpadding=2>");
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                response.getWriter().write("<TR>");
-                SectionType key = entry.getKey();
-                switch (key) {
-                    case OBJECTIVE:
-                    case PERSONAL:
-                        response.getWriter().write("<TD>" + key + "</TD>");
-                        response.getWriter().write("<TD>" + ((TextSection) entry.getValue()).getContent() + "</TD>");
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        response.getWriter().write("<TD>" + key + "</TD>");
-                        List<String> items = ((ListSection) entry.getValue()).getItems();
-                        response.getWriter().write("<TD>");
-                        for (String item : items) {
-                            response.getWriter().write("<LI>" + item + "</LI>");
-                        }
-                        response.getWriter().write("</TD>");
-                }
-                response.getWriter().write("</TR>");
-            }
-            response.getWriter().write("</TABLE>");
-        }
-        response.getWriter().write("<BR><BR>");
+        response.getWriter().write("<TR>");
+        response.getWriter().write("<TD>" + resume.getUuid() + "</TD>");
+        response.getWriter().write("<TD>" + resume.getFullName() + "</TD>");
+        response.getWriter().write("</TR>");
     }
 }
